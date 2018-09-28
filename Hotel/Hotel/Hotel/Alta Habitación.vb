49fc7bb8e79fd49f5902b338dbf38dbeacda7da8
Matias Turra <matias.turra@gmail.com>
2018-09-28 08:45:18 -0300

    28/9

--- Hotel/Hotel/Hotel/Alta Habitación.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
-
-    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
-
-    End Sub
-End Class
